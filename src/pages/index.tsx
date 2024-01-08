47fd0c1fcc26a1d14f6e75a917596eba12f29393
Matheus Torres <matheustorres@poli.ufrj.br>
2024-01-08 11:48:41 -0300

    fix: telefone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -76,16 +76,7 @@ const Home: NextPage = () => {
               </svg>
             </Flex>
           </Link>
-
-          <Flex
-            direction="col"
-            className="max-w-[412px] md:max-w-[200px] xl:max-w-[412px] lg:max-w-[352px] gap-5 2xl:gap-8 rounded-sm bg-white pb-6 pt-4 px-6 2xl:p-6"
-          >
-            <p className="text-[20px] 2xl:text-[24px] font-[500] leading-[29px] text-neutral-pure900">
-              Faça a sua inscrição!
-            </p>
-            <Form />
-          </Flex>
+          <Form />
         </Flex>
       </Flex>
       <Flex
