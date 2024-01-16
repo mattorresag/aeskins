b4719deef5c3f6934c7d337fc5a49bc126219790
Matheus Torres <matheustorres@poli.ufrj.br>
2024-01-16 20:18:55 -0300

    feat: home BG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,19 @@ import { NextPage } from "next/types";
 import { Layout } from "../Layout/Layout";
 import { Flex } from "../components/Flex/Flex";
 import Image from "next/image";
-import Form from "../features/Form/Form";
-import { Dates } from "../features/Dates/Dates";
-import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <Layout>
-
+      <Flex className="md:bg-background-home
+      bg-background-homeMobile
+    bg-no-repeat
+    bg-cover
+    md:bg-[center_left_-350px]
+    lg:bg-center
+    bg-center w-full bg-primary-pure relative h-[652px] min-[433px]:h-[750px] min-[400px]:h-[700px] min-[1800px]:h-[800px] min-[1921px]:h-[1000px] pt-[64px]  md:py-[147px]  md:pl-[5%] xl:pl-[160px]   flex-col md:flex-row">
+        .
+      </Flex>
     </Layout>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,10 @@ module.exports = {
           normal: "400",
         },
       },
+      backgroundImage: {
+        "background-home": "url('/assets/banner.png')",
+        "background-homeMobile": "url('/assets/bannerMobile.png')",
+      },
       boxShadow: {
         form: "0px 4px 24px 0px rgba(207, 214, 219, 0.08)",
       },
