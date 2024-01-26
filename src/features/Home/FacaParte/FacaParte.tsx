abeb4430274db5b222e39351bd04c2dd0497fa92
Matheus Torres <matheustorres@poli.ufrj.br>
2024-01-26 17:17:56 -0300

    Update FacaParte component with new images

--- src/features/Home/FacaParte/FacaParte.tsx
+++ src/features/Home/FacaParte/FacaParte.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import { Flex } from "../../../components/Flex/Flex";
 import Image from "next/image";
 import useWindowWidth from "../../../hooks/useWindowWidth";
-import Icons from "../../../../public/assets/icons";
+import FacaParteImage from "../../../../public/assets/facaParte.png";
+import FacaParte1 from "../../../../public/assets/faca1.png";
+import FacaParte2 from "../../../../public/assets/faca2.png";
+import FacaParte3 from "../../../../public/assets/faca3.png";
 
 export const FacaParte = () => {
   const width = useWindowWidth();
@@ -10,7 +13,7 @@ export const FacaParte = () => {
     <Flex className="p-[5%] w-full gap-2 lg:p-16 bg-black flex-col lg:flex-row lg:h-[677px]">
       <Flex className="w-full h-[160px] min-[426px]:h-[250px] md:h-[300px] lg:h-[540px] lg:w-[50%]">
         <Image
-          src="/assets/facaParte.png"
+          src={FacaParteImage.src}
           width={1000}
           height={540}
           alt="Faca Parte"
@@ -39,7 +42,7 @@ export const FacaParte = () => {
           className="w-full lg:w-[calc(50%-16px)] h-[160px] lg:h-[266px]"
         >
           <Image
-            src="/assets/faca1.png"
+            src={FacaParte1.src}
             width={800}
             height={266}
             alt="Faca Parte 1"
@@ -52,7 +55,7 @@ export const FacaParte = () => {
           className="w-full lg:w-[calc(50%-16px)] h-[160px] lg:h-[266px]"
         >
           <Image
-            src="/assets/faca2.png"
+            src={FacaParte2.src}
             width={800}
             height={266}
             alt="Faca Parte 2"
@@ -65,7 +68,7 @@ export const FacaParte = () => {
           className="w-full lg:w-[calc(50%-16px)] h-[160px] lg:h-[266px]"
         >
           <Image
-            src="/assets/faca3.png"
+            src={FacaParte3.src}
             width={800}
             height={266}
             alt="Faca Parte 3"
