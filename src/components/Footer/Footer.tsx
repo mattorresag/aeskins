a7a8423ce173d33086523be52a3a31639e726877
Matheus Torres <matheustorres@poli.ufrj.br>
2024-02-16 13:49:10 -0300

    refac: remove depoimentos from footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -117,13 +117,6 @@ export const Footer = (): JSX.Element => {
                     </p>
                   </a>
                 </Link>
-                <Link href="/quem-somos" passHref>
-                  <a>
-                    <p className="text-[20px] text-white leading-[30px] cursor-pointer">
-                      Depoimentos
-                    </p>
-                  </a>
-                </Link>
                 <Link href="/encontre-uma-clinica" passHref>
                   <a>
                     <p className="text-[20px] text-white leading-[30px] cursor-pointer">
