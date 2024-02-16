a3cc526c1bf4de720402b15f563e63967011bbf5
Matheus Torres <matheustorres@poli.ufrj.br>
2024-02-16 13:48:31 -0300

    refac: video blur on the back

--- src/features/Home/Banner/Banner.tsx
+++ src/features/Home/Banner/Banner.tsx
@@ -4,14 +4,19 @@ import AutoPlaySilentVideo from "../../../hooks/usePlayVideo";
 export const Banner = () => {
   return (
     <Flex className="w-full flex-col md:flex-row">
-      <Flex direction="col" className="w-full">
+      <Flex direction="col" className="w-full relative">
         <Flex className=" w-full h-full max-h-[calc(100vh-80px)] z-[10] justify-center ">
           <AutoPlaySilentVideo
             className="w-full h-full"
             video="/assets/video_banner.mp4"
           />
         </Flex>
+        <div className="absolute top-0  transform  z-[1] w-full h-full  overflow-hidden">
+          <div className="w-full h-full blur-xl  filter opacity-100 scale-[1.25]">
+            <AutoPlaySilentVideo className="w-full h-full" video="/assets/video_banner.mp4" />
+          </div>
+        </div>
       </Flex>
-    </Flex>
+    </Flex >
   );
 };
